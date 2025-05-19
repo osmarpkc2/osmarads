dbad9f9d4d80a6d23c0d212b8f731b6cf00ba4ad
Osmar <osmaroliveira570@gmail.com>
2025-05-19 01:05:15 -0300

    fix: atualiza configurações para deploy no Render

---
+++ gunicorn_config.py
@@ -0,0 +1,26 @@
+# Nome do arquivo da aplicação
+app = 'app:app'
+
+# Número de workers (2 * núcleos da CPU + 1)
+workers = 3
+
+# Número de threads por worker
+threads = 2
+
+# Endereço e porta
+bind = '0.0.0.0:10000'
+
+# Timeout
+# timeout = 120
+
+# Nível de log
+loglevel = 'info'
+
+# Acesso ao log
+accesslog = '-'  # Saída para stdout
+
+# Log de erros
+errorlog = '-'  # Saída para stderr
+
+# Recarregar código automaticamente (apenas para desenvolvimento)
+# reload = True
